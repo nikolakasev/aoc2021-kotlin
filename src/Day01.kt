7df8a8b7eb5fc13b618ee8de4dd5d3c9e7d4e9cd
Nikola Kasev <nikola.kasev@gmail.com>
2022-02-28 17:24:12 +0100

    solve D1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,22 @@
+import kotlin.collections.sumOf
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        // the depth measurement increases
+        val transform: (List<Int>) -> Int = { if (it.last() > it.first()) 1 else 0}
+        // turn from a list of strings to a list of integers
+        val measurements = input.map { it.toInt() }
+
+        // make pairs of measurements, count all the times a measurement increases
+        return measurements.windowed(2, 1).sumOf(transform)
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
-    }
+        val transform: (List<List<Int>>) -> Int = {if (it.first().sum() < it.last().sum()) 1 else 0}
+        val measurements = input.map { it.toInt() }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+        return measurements.windowed(3, 1).windowed(2, 1).sumOf(transform)
+    }
 
     val input = readInput("Day01")
     println(part1(input))
